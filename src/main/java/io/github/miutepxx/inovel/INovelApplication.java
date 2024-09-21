d884b309e4fce581e004e374a4bef8b828f72851
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-09-21 16:14:34 +0800

    项目初始化
    使用 Spring Initializr 来初始化

---
+++ src/main/java/io/github/miutepxx/inovel/INovelApplication.java
@@ -0,0 +1,13 @@
+package io.github.miutepxx.inovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class INovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(INovelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/miutepxx/inovel/INovelApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.miutepxx.inovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class INovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
